2f491c527e9c3586ac6f749e55f69ba83b020c7d
HaninHaidrah <haneenhadey@gmail.com>
2021-11-25 13:50:28 +0200

    auth test done

--- src/models/index.js
+++ src/models/index.js
@@ -7,7 +7,7 @@ const Collection = require("../models/data-collection");
 
 const { Sequelize, DataTypes } = require("sequelize");
 
-const DATABASE_URL = "postgres://localhost:5432/midProject2";
+const DATABASE_URL = "postgres://localhost:5432/housy";
 
 let sequelizeOptions =
   process.env.NODE_ENV === "production"
