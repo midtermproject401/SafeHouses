bae0a78bc2d8da2aa467b9a0e2e2677bf8715150
MohammadAljadayh <mjedayh@gmail.com>
2022-01-04 13:24:57 +0200

    update

--- public/js/main.js
+++ public/js/main.js
@@ -15,31 +15,27 @@ rent.addEventListener('click', (e) => {
   
 
 
-//  if (username.includes("(owner)"))
-//  {
-//   let msg1 ='plese check the House details and payment method in the below link';
-//    let msg2 =`https://safe-housy.herokuapp.com/rent/${Advname}`;
-//   msg1 = msg1.trim();
-//    if (!msg1) {
-//     return false;
-//   }
-//   msg2 = msg2.trim();
-//   if (!msg2) {
-//    return false;
-//  }
-//   // Emit message to server
-//   socket.emit('chatMessage', msg1);
-//   socket.emit('chatMessage', msg2);
-//   // Clear input
-//   e.target.elements.msg1.value = '';
-//   e.target.elements.msg1.focus();
-//   e.target.elements.msg2.value = '';
-//   e.target.elements.msg2.focus();
-// }
-// else {
-//   alert("You Can't rent the house until agreement from the owner:) ")
-// }
-// }
+
+  let msg1 ='plese check the House details and payment method in the below link';
+   let msg2 =`https://safe-housy.herokuapp.com/rent/${Advname}`;
+  msg1 = msg1.trim();
+   if (!msg1) {
+    return false;
+  }
+  msg2 = msg2.trim();
+  if (!msg2) {
+   return false;
+ }
+  // Emit message to server
+  socket.emit('chatMessage', msg1);
+  socket.emit('chatMessage', msg2);
+  // Clear input
+  e.target.elements.msg1.value = '';
+  e.target.elements.msg1.focus();
+  e.target.elements.msg2.value = '';
+  e.target.elements.msg2.focus();
+
+}
 
 );
 const socket = io();
