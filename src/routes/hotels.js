4e13427bf0b6475d655dd2a1a7952588cd079c5e
HaninHaidrah <haneenhadey@gmail.com>
2022-01-04 21:56:24 +0200

    auth

--- src/routes/hotels.js
+++ src/routes/hotels.js
@@ -13,6 +13,8 @@ router.get("/hotel/:id", handleGetOne);
 router.delete("/hotel/:id", handleDelete);
 router.put("/:model/:id", handleUpdate);
 router.post("/hotel/:id/rooms", handleCreateRooms);
+router.get("/hotel/:id/rooms/:roomid", handleGetOneRoom);
+
 router.get("/hotel/:id/rooms", handleGetRooms);
 
 async function handleUpdate(req, res) {
@@ -42,6 +44,13 @@ async function handleGetOne(req, res) {
   const id = req.params.id;
   const allRecords = await hotel.get(id);
   res.status(200).json(allRecords);
+}
+async function handleGetOneRoom(req, res) {
+  const id = req.params.roomid;
+  console.log(id,"roomid");
+  const allRecords = await room.get(id);
+  res.status(200).json(allRecords);
+  // res.send('hi')
 }
 async function handleCreateRooms(req, res) {
   const obj = req.body;
